9cb05e8e809e17c287ab938b2a439fecfce891da
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-04-14 16:21:23 -0300

    Alter cors policies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,9 +8,7 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use([userRouter, imvRouter]);
