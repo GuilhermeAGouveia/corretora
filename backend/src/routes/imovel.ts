da6650b2662ea71f4e12f24ca8df2fffab83c650
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-04-19 18:57:11 -0300

    Remove commented code after refactor

--- backend/src/routes/imovel.ts
+++ backend/src/routes/imovel.ts
@@ -1,19 +1,6 @@
 import ImovelController from "../controller/ImovelController";
 import UniqueKeyRoutesFactory from "./UniqueKeyRoutesFactory";
 
-/* const router = Router();
-
-const rootPath = "/imovel";
-
-router.get(`${rootPath}/`, ImovelController.default);
-router.get(`${rootPath}/count`, ImovelController.count);
-router.get(`${rootPath}/get/:cod`, ImovelController.getByCod);
-router.get(`${rootPath}/all`, ImovelController.getAll);
-
-router.post(`${rootPath}/`, ImovelController.insert);
-
-router.delete(`${rootPath}/:cod`, ImovelController.delete); */
-
 const routesFactory = new UniqueKeyRoutesFactory("/imovel", ImovelController);
 
 export default routesFactory.createRoutes();
--- backend/src/routes/pessoa.ts
+++ backend/src/routes/pessoa.ts
@@ -5,49 +5,11 @@ import LocatarioController from "../controller/LocatarioController";
 import UniqueKeyRoutesFactory from "./UniqueKeyRoutesFactory";
 
 
-
-const rootPath = "/pessoa";
-
-//locador
-/* router.get(`${rootPath}/locador/`, LocadorController.default);
-router.get(`${rootPath}/locador/all`, LocadorController.getAll);
-router.get(`${rootPath}/locador/count`, LocadorController.count);
-router.get(`${rootPath}/locador/get/:cod`, LocadorController.getByCod);
-
-router.post(`${rootPath}/locador/`, LocadorController.insert);
-
-router.delete(`${rootPath}/locador/:cod`, LocadorController.delete);
- */
-
 const routerLocador = new UniqueKeyRoutesFactory("/pessoa/locador", LocadorController);
 const routesLocador = routerLocador.createRoutes();
 
-//corretor
-/* router.get(`${rootPath}/corretor/`, CorretorController.default);
-router.get(`${rootPath}/corretor/all`, CorretorController.getAll);
-router.get(`${rootPath}/corretor/count`, CorretorController.count);
-router.get(`${rootPath}/corretor/get/:cod`, CorretorController.getByCod);
-
-router.post(`${rootPath}/corretor`, CorretorController.insert);
-
-router.delete(`${rootPath}/corretor/:cod`, CorretorController.delete); */
-
 const routerCorretor = new UniqueKeyRoutesFactory("/pessoa/corretor", CorretorController);
 const routesCorretor = routerCorretor.createRoutes();
-
-//locatario
-/* router.get(`${rootPath}/locatario/`, LocatarioController.default);
-router.get(`${rootPath}/locatario/all`, LocatarioController.getAll);
-router.get(`${rootPath}/locatario/count`, LocatarioController.count);
-router.get(`${rootPath}/locatario/get/:cod`, LocatarioController.getByCod);
-
-router.post(
-  `${rootPath}/locatario`,
-  convertStringToDateMiddleware,
-  LocatarioController.insert
-);
-
-router.delete(`${rootPath}/locatario/:cod`, LocatarioController.delete); */
 
 const routerLocatario = new UniqueKeyRoutesFactory("/pessoa/locatario", LocatarioController);
 const routesLocatario = routerLocatario.createRoutes();
