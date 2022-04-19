4daf3b3f5dbd29af3976d0a5cd183eef8ce157ec
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-04-19 19:04:12 -0300

    Remove commented code after refactor

--- backend/src/routes/associado.ts
+++ backend/src/routes/associado.ts
@@ -1,18 +1,6 @@
 import AssociadoController from "../controller/AssociadoController";
 import MultiKeyRoutesFactory from "./MultiKeyRoutesFactory";
 
-/* const router = Router();
-
-const rootPath = "/associado";
-
-router.get(`${rootPath}/`, AssociadoController.default);
-router.get(`${rootPath}/count`, AssociadoController.count);
-router.get(`${rootPath}/get`, AssociadoController.getByCod);
-router.get(`${rootPath}/all`, AssociadoController.getAll);
-
-router.post(`${rootPath}/`, convertStringToDateMiddleware ,AssociadoController.insert);
-
-router.delete(`${rootPath}/`, AssociadoController.delete); */
 
 const routesFactory = new MultiKeyRoutesFactory("/associado", AssociadoController);
 
