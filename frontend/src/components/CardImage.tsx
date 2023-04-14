4f7f09d8d8657acf2c63adb517f238f887202373
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-04-14 14:27:25 -0300

    fix: height bug in cardimage

--- frontend/src/components/CardImage.tsx
+++ frontend/src/components/CardImage.tsx
@@ -61,7 +61,7 @@ export const ImagePlaceholder = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 300px;
   background-color: ${colors.white};
   display: flex;
   justify-content: center;
