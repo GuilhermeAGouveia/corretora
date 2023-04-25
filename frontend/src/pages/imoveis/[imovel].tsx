abc355c1966ae1dd9909e60a402a5158ce88ba00
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-04-25 10:36:14 -0300

    refactor: image view at detail imovel screen

--- frontend/src/pages/imoveis/[imovel].tsx
+++ frontend/src/pages/imoveis/[imovel].tsx
@@ -1,10 +1,12 @@
-import { MoreVert } from "@mui/icons-material";
+import { Close, MoreVert } from "@mui/icons-material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import RoomIcon from "@mui/icons-material/Room";
 import { Typography } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 import { capitalize } from "lodash";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
+import { useState } from "react";
 import styled from "styled-components";
 import avatarPNG from "../../assets/images/avatar.png";
 import casaPNG from "../../assets/images/casa.png";
@@ -44,137 +46,195 @@ interface ImovelProps {
 }
 
 export default function Imovel({ imovel, locador }: ImovelProps) {
+  const [imageSelected, setImageSelected] = useState("");
   const { isMobileView } = useDeviceDetect();
   return (
     <ImovelDetailContainer>
       <TopBar pageName="Detalhes do Imóvel" />
-      <TopContainer>
-        <PropietarioContainer>
-          <PropietarioAvatar>
-            <Image src={avatarPNG} alt="Avatar" width={30} />
-          </PropietarioAvatar>
-          <PropietarioInfo>
-            <Typography fontSize={"small"} fontFamily={"Lato, sans-serif"}>
-              Anunciado por
-            </Typography>
-            <Typography
-              fontSize={"medium"}
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={"bold"}
-              color={colors.primary}
-            >
-              {locador?.firstName} {locador?.lastName}
-            </Typography>
-          </PropietarioInfo>
-        </PropietarioContainer>
-        <ActionsContainer>
-          <FavoriteBorderIcon
-            sx={{
-              color: colors.primary,
-              fontSize: 20,
-            }}
-          />
-          <MoreVert
-            sx={{
-              color: colors.primary,
-              fontSize: 20,
-            }}
-          />
-        </ActionsContainer>
-      </TopContainer>
-      <BodyContainer isMobile={isMobileView}>
-        <ImageContainer>
-          <MainImageContainer>
-            <Image src={casaPNG} alt="Casa" layout="fill" objectFit="cover" />
-          </MainImageContainer>
-        </ImageContainer>
-        <ImovelDescriptionContainer isMobile={isMobileView}>
-          <ImovelTittleContainer>
-            <Typography
-              fontSize={"large"}
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={"bold"}
-              color={colors.primary}
-              sx={{
-                width: "30%",
-                marginRight: "10px",
-              }}
-            >
-              {capitalize(imovel?.type as string)} em
-            </Typography>
-            <LocalContainer>
+      <FullScreenBox>
+        <TopContainer>
+          <PropietarioContainer>
+            <PropietarioAvatar>
+              <Image src={avatarPNG} alt="Avatar" width={25} />
+            </PropietarioAvatar>
+            <PropietarioInfo>
               <Typography
-                sx={{
-                  position: "relative",
-                  left: 10,
-                  top: 5,
-                  fontWeight: 500,
-                }}
+                fontSize={"small"}
+                fontFamily={"Lato, sans-serif"}
+                lineHeight={"10px"}
               >
-                {imovel?.address}, {imovel?.city}, {imovel?.state}
+                Anunciado por
               </Typography>
-              <RoomIcon
-                sx={{
-                  color: colors.primary,
-                  fontSize: 20,
-                  position: "absolute",
-                  left: 5,
-                  top: 5,
+              <Typography
+                fontSize={"medium"}
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={"bold"}
+                color={colors.primary}
+              >
+                {locador?.firstName} {locador?.lastName}
+              </Typography>
+            </PropietarioInfo>
+          </PropietarioContainer>
+          <ActionsContainer>
+            <FavoriteBorderIcon
+              sx={{
+                color: colors.primary,
+                fontSize: 20,
+              }}
+            />
+            <MoreVert
+              sx={{
+                color: colors.primary,
+                fontSize: 20,
+              }}
+            />
+          </ActionsContainer>
+        </TopContainer>
+        <BodyContainer isMobile={isMobileView}>
+          <ImageContainer>
+            <MainImageContainer>
+              <Image
+                src={casaPNG}
+                alt="Casa"
+                style={{
+                  objectFit: "cover",
                 }}
               />
-            </LocalContainer>
-          </ImovelTittleContainer>
-          <ImovelDescriptionTextContainer>
-            <Typography
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={300}
-              fontSize={18}
-            >
-              Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 1 cozinha, 1
-            </Typography>
-          </ImovelDescriptionTextContainer>
-        </ImovelDescriptionContainer>
+            </MainImageContainer>
+            <ImageSelectorContainer>
+              <ImageSelector
+                layoutId="image1"
+                onClick={() => setImageSelected("image1")}
+              >
+                <Image src={casaPNG} alt="Casa" width={50} />
+              </ImageSelector>
+              <ImageSelector
+                layoutId="image2"
+                onClick={() => setImageSelected("image2")}
+              >
+                <Image src={casaPNG} alt="Casa" width={50} />
+              </ImageSelector>
+              <ImageSelector
+                layoutId="image3"
+                onClick={() => setImageSelected("image3")}
+              >
+                <Image src={casaPNG} alt="Casa" width={50} />
+              </ImageSelector>
+              <ImageSelector
+                layoutId="image4"
+                onClick={() => setImageSelected("image4")}
+              >
+                <Image src={casaPNG} alt="Casa" width={50} />
+              </ImageSelector>
+            </ImageSelectorContainer>
+            <AnimatePresence>
+              {imageSelected && (
+                <FocusImage layoutId={imageSelected}>
+                  <CloseImageButton onClick={() => setImageSelected("")}>
+                    <Close
+                      sx={{
+                        color: colors.primary,
+                        fontSize: 20,
+                      }}
+                    />
+                  </CloseImageButton>
+                  <Image
+                    src={casaPNG}
+                    alt="Casa"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </FocusImage>
+              )}
+            </AnimatePresence>
+          </ImageContainer>
+          <ImovelDescriptionContainer isMobile={isMobileView}>
+            <ImovelTittleContainer>
+              <Typography
+                fontSize={"large"}
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={"bold"}
+                color={colors.primary}
+                sx={{
+                  width: "30%",
+                  marginRight: "10px",
+                }}
+              >
+                {capitalize(imovel?.type as string)} em
+              </Typography>
+              <LocalContainer>
+              <RoomIcon
+                  sx={{
+                    color: colors.primary,
+                    fontSize: 20,
+                  }}
+                />
+                <Typography
+                  fontSize={"small"}
+                  sx={{
+                    position: "relative",
+                    fontWeight: 500,
+                    color: "#343434",
+                  }}
+                >
+                  {imovel?.address}, {imovel?.city}, {imovel?.state}
+                </Typography>
+              
+              </LocalContainer>
+            </ImovelTittleContainer>
+            <ImovelDescriptionTextContainer>
+              <Typography
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={300}
+                fontSize={18}
+              >
+                Casa com 3 quartos, 2 banheiros, 1 vaga de garagem, 1 cozinha, 1
+              </Typography>
+            </ImovelDescriptionTextContainer>
+          </ImovelDescriptionContainer>
 
-        <ButtonsImovelContainer isMobile={isMobileView}>
-          <ButtonImovel>
-            <Typography
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={300}
-              fontSize={24}
+          <ButtonsImovelContainer isMobile={isMobileView}>
+            <ButtonImovel>
+              <Typography
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={300}
+                fontSize={24}
+              >
+                R$ {imovel?.mensalidade}
+              </Typography>
+              <Typography
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={600}
+                fontSize={18}
+              >
+                Aluguel
+              </Typography>
+            </ButtonImovel>
+            <ButtonImovel
+              style={{
+                backgroundColor: "white",
+                color: colors.primary,
+              }}
             >
-              R$ {imovel?.mensalidade}
-            </Typography>
-            <Typography
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={600}
-              fontSize={18}
-            >
-              Aluguel
-            </Typography>
-          </ButtonImovel>
-          <ButtonImovel
-            style={{
-              backgroundColor: colors.white,
-              color: colors.primary,
-            }}
-          >
-            <Typography
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={300}
-              fontSize={24}
-            >
-              R$ {imovel?.price}
-            </Typography>
-            <Typography
-              fontFamily={"Lato, sans-serif"}
-              fontWeight={600}
-              fontSize={18}
-            >
-              Comprar
-            </Typography>
-          </ButtonImovel>{" "}
-        </ButtonsImovelContainer>
-      </BodyContainer>
+              <Typography
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={300}
+                fontSize={24}
+              >
+                R$ {imovel?.price}
+              </Typography>
+              <Typography
+                fontFamily={"Lato, sans-serif"}
+                fontWeight={600}
+                fontSize={18}
+              >
+                Comprar
+              </Typography>
+            </ButtonImovel>{" "}
+          </ButtonsImovelContainer>
+        </BodyContainer>
+      </FullScreenBox>
     </ImovelDetailContainer>
   );
 }
@@ -182,7 +242,6 @@ export default function Imovel({ imovel, locador }: ImovelProps) {
 const ImovelDetailContainer = styled.div`
   position: relative;
   margin: 0 auto;
-  min-height: 100vh;
 `;
 
 const TopContainer = styled.div`
@@ -193,14 +252,14 @@ const TopContainer = styled.div`
   justify-content: space-between;
   width: 100%;
   height: 50px;
-  padding: 10px;
+  padding: 7.5px 10px;
 `;
 
 const PropietarioContainer = styled.div`
   position: relative;
   display: flex;
-  height: 100%;
-  width: 40%;
+  height: auto;
+  width: auto;
   align-items: center;
   justify-content: center;
 `;
@@ -208,8 +267,8 @@ const PropietarioContainer = styled.div`
 const PropietarioAvatar = styled.div`
   position: relative;
   display: flex;
-  min-height: 40px;
-  min-width: 40px;
+  min-height: 35px;
+  min-width: 35px;
   border-radius: 50%;
   background-color: ${colors.primary};
   align-items: center;
@@ -222,7 +281,6 @@ const PropietarioInfo = styled.div`
   margin-left: 10px;
   display: flex;
   flex-direction: column;
-  height: 100%;
   width: 100%;
   align-items: flex-start;
   justify-content: center;
@@ -240,30 +298,79 @@ const ActionsContainer = styled.div`
 
 const ImageContainer = styled.div`
   position: relative;
-  display: flex;
-  height: calc(60vh - 100px);
+  display: block;
+  height: 40%;
   width: 100%;
+  padding: 10px;
   align-items: center;
   justify-content: center;
-  overflow: hidden;
-  margin: 20px auto;
 `;
 const MainImageContainer = styled("div")`
   position: relative;
   display: flex;
-  height: 100%;
+  height: 80%;
   width: 100%;
-  margin: 20px;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
+`;
+
+const ImageSelectorContainer = styled.div`
+  position: relative;
+  display: flex;
+  height: 20%;
+  padding: 10px;
+  width: 100%;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 10px;
+  overflow: hidden;
+`;
+
+const ImageSelector = styled(motion.div)`
+  position: relative;
+  display: flex;
+  height: 50px;
+  height: 50px;
+  overflow: hidden;
+  border-radius: 2px;
+`;
+
+const FocusImage = styled(motion.div)`
+  position: fixed;
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.95);
+  overflow: hidden;
+  inset: 0;
+  z-index: 2;
+`;
+
+const CloseImageButton = styled.button`
+  position: absolute;
+  display: flex;
+  height: 30px;
+  width: 30px;
+  border: none;
+  border-radius: 50%;
+  background-color: ${colors.secondary};
+  color: ${colors.white};
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  top: 10px;
+  right: 10px;
 `;
 
 const ButtonImovel = styled.button`
   position: relative;
   display: flex;
   flex-direction: column;
-  height: 100px;
-  width: 130px;
+  height: 150px;
+  width: 180px;
   border: none;
   border-radius: 5px;
   background-color: ${colors.primary};
@@ -277,15 +384,11 @@ const ButtonImovel = styled.button`
 const ButtonsImovelContainer = styled.div<{
   isMobile: boolean;
 }>`
-  position: ${(props) => (props.isMobile ? "absolute" : "relative")};
+  position: relative;
   display: flex;
-  flex-direction: ${(props) => (props.isMobile ? "row" : "column")};
-  height: ${(props) => (props.isMobile ? "160px" : "auto")};
-  width: ${(props) => (props.isMobile ? "100%" : "400px")};
   align-items: center;
   justify-content: center;
   overflow: hidden;
-  bottom: ${(props) => (props.isMobile ? "-160px" : "auto")};
 `;
 
 const ImovelTittleContainer = styled.div`
@@ -302,27 +405,21 @@ const ImovelTittleContainer = styled.div`
 const LocalContainer = styled.div`
   position: relative;
   display: flex;
-  flex-direction: row;
   width: 100%;
   align-items: center;
   justify-content: space-between;
   overflow: hidden;
+  gap: 10px;
   padding: 10px;
-  padding-top: 20px;
   background: ${colors.white};
   border-radius: 5px;
   border: 1px solid ${colors.primary};
 `;
 
 const BodyContainer = styled.div<{ isMobile: boolean }>`
-  position: relative;
-  display: flex;
-  flex-direction: ${({ isMobile }) => (isMobile ? "column" : "row")};
-  height: 70vh;
-  width: 100%;
-  align-items: ${(props) => (props.isMobile ? "center" : "flex-start")};
-  margin-top: ${(props) => (props.isMobile ? "0px" : "10vh")};
-  justify-content: center;
+  position: block;
+  height: auto;
+  width: auto;
 `;
 
 const ImovelDescriptionTextContainer = styled.div`
@@ -333,12 +430,17 @@ const ImovelDescriptionTextContainer = styled.div`
   justify-content: center;
   overflow: hidden;
   padding: 10px 20px;
-  margin-top: 20px;
 `;
 
-const ImovelDescriptionContainer = styled.div<{isMobile: boolean}>`
+const ImovelDescriptionContainer = styled.div<{ isMobile: boolean }>`
   position: relative;
   display: flex;
-  flex-direction: column; 
-  width: ${({isMobile}) => isMobile ? "100%" : "50%"};
+  flex-direction: column;
+`;
+
+const FullScreenBox = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: ${colors.white};
 `;
